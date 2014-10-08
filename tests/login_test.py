5f433349d87caf1a53f9422d0910ebb08d86f8b5
Kevin Brandstatter <kjbrandstatter@gmail.com>
2014-10-08 14:06:13 -0500

    updated patching

--- tests/login_test.py
+++ tests/login_test.py
@@ -5,20 +5,20 @@ from login import login
 class login_test(unittest.TestCase):
     def setUp(self):
         self.loginclass = login()
-
-    def test_successful_login(self):
-        pass
-
-    @patch('login.login.get_user')
-    def test_correct_type_on_login(self, mock_get_user):
+        self.patcher = patch('login.login.get_user')
+        mock = self.patcher.start()
         user = MagicMock()
         user.uname = "manager"
         user.passhash = "$2a$12$CyLyLDPA5NFTY48o3fANQOEsni38JgHBk3FNwdUFd1OwYMBZxN146"
         user.utype = "manager"
-        mock_get_user.return_value = user
+        mock.return_value = user
+
+    def test_correct_type_on_login(self):
         utype = self.loginclass.login_user("manager", "password")
         self.assertEqual(utype, "manager")
 
-    def test_unsuccessful_login(self):
+    @patch('login.login.get_user')
+    def test_unsuccessful_login(self, mock_get_user):
+        mock_get_user.return_value = None
         utype = self.loginclass.login_user("manager", "password")
         self.assertEqual(utype, None)
