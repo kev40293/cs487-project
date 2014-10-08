d4e1673a21ff2fe19322153363e1190317bda410
Kevin Brandstatter <kjbrandstatter@gmail.com>
2014-10-08 13:45:56 -0500

    added support for sessions and stuff

--- main.py
+++ main.py
@@ -2,19 +2,24 @@
 import web
 
 from login import login
+web.config.debug = False
 
 urls = (
         '/', 'index',
         '/login', 'login'
         )
 render = web.template.render("templates")
+app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'loggedin' : False})
 
 class index():
     def GET(self):
         # Check if logged in
+        if session.loggedin:
+            return "You are logged in"
         # If not logged in
-        return render.login()
+        else:
+            return render.login()
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
     app.run()
