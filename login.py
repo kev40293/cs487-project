551551a2668e440b30c566731b0f4638c976cec5
Kevin Brandstatter <kjbrandstatter@gmail.com>
2014-10-08 11:51:26 -0500

    basic testing framework added (nosetests)

--- login.py
+++ login.py
@@ -9,4 +9,8 @@ class login:
 
     def POST(self):
         upass = web.input(username=None)
+        user_type = login_user(upass.username, user.password)
         return str(upass.username) + " tried to log in"
+
+    def login_user(self, user, password):
+        pass
---
+++ tests/login_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+class login_test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        assert (True)
+
+    def test_successful_login(self):
+        pass
+
+    def test_correct_type_on_login(self):
+        pass
+
+    def test_unsuccessful_login(self):
+        pass
